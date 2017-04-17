b67f830600f8d02d2408609dc6ed70cd7ce92995
Zachary Bergmann <zbbergma@gmail.com>
2017-04-17 13:29:06 -0500

    fix linting issues with airbnb linter installed

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MongoClient = require('mongodb').MongoClient;
 
 let db;
 MongoClient.connect(process.env.MONGOURI, (err, database) => {
   if (err) {
     console.error(err);
   } else {
-    database = db;
-    app.listen(PORT, function() {
+    db = database;
+    app.listen(PORT, () => {
       console.log(`Server is running on ${PORT}`);
     });
   }
 });
 
-app.use(bodyParser.urlencoded( { extended: true } ));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
 });
