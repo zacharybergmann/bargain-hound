c21baa3acbdf4e6185c3eadba627a8c1c9850536
Zachary Bergmann <zbbergma@gmail.com>
2017-04-17 18:54:13 -0500

    have hard coded stocks appearing on the screen. continue building out components for stock dashboard

--- public/app.js
+++ public/app.js
@@ -1,14 +1,33 @@
-const app = angular.module('app', [])
+const app = angular.module('app', [
+  'ngRoute',
+  'ngResource',
+])
+.controller('DashboardController', ($scope) => {
+  $scope.stocks = [
+    {
+      name: 'AAPL',
+      price: '$140.00',
+    },
+    {
+      name: 'SLB',
+      price: '$80.00',
+    },
+  ];
+})
 .config(($routeProvider) => {
   $routeProvider
-    .when('/mystocks', {
-      templateUrl: 'app/templates/my_stock.template.html',
-      controller: 'MyStockController'
+    .when('/signup', {
+      templateUrl: 'app/templates/signup.template.html',
+      controller: 'SignUpController',
     })
-    .when('/stocksummary', {
-      templateUrl: 'app/templates/stock_summary.template.html',
-      controller: 'SummaryController'
+    .when('/login', {
+      templateUrl: 'app/templates/login.template.html',
+      controller: 'LogInController',
+    })
+    .when('/mydash', {
+      templateUrl: 'app/templates/my_stock.template.html',
+      controller: 'MyStockController',
     }).otherwise({
-      redirectTo: '/'
+      redirectTo: '/',
     });
 });
