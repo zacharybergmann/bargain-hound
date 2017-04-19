0882c5f1c9385cfe25c2a49a19c717497a5f4a02
Zachary Bergmann <zbbergma@gmail.com>
2017-04-19 10:58:46 -0500

    get stocks that the user is interested in to render in mydash. also, login works

--- public/app/dashboard/dashboard.component.js
+++ public/app/dashboard/dashboard.component.js
@@ -3,20 +3,17 @@ angular
   .component('dashboard', {
     templateUrl: 'app/dashboard/dashboard.template.html',
     controller: ['$http', function DashBoardController($http) {
-      this.orderProp = 'age';
       const basicData = ['YHOO', 'SLB', 'HAL', 'GE', 'GD', 'VZ', 'PG', 'AAL', 'LUV', 'AMD', 'BP', 'C',
         'WFC', 'CSCO', 'CMG', 'DIS', 'F', 'GM', 'FB', 'GIS', 'IBKC', 'HD', 'JPM', 'K', 'LMT', 'MMM', 'PEP',
         'PFE', 'PM', 'RCL', 'TEVA', 'WMT', 'XON'];
 
       let makeEndpoint = '/financials';
       basicData.forEach((stock) => { makeEndpoint += `/${stock}`; });
-      console.log(makeEndpoint, 'this is your endpoint sire');
       $http.get(makeEndpoint)
         .then((resp) => {
-          console.log(resp.data.data, 'this is doge response!');
           const fixedData = resp.data.data;
           fixedData.pop();
           this.stocks = fixedData;
-        }).catch(err => console.error(err));
+        }).catch(err => err);
     }],
   });
--- public/app/mydash/mydash.component.js
+++ public/app/mydash/mydash.component.js
@@ -2,21 +2,22 @@ angular
   .module('mydash')
   .component('mydash', {
     templateUrl: 'app/mydash/mydash.template.html',
-    controller: ['$http', function DashBoardController($http) {
-      this.orderProp = 'age';
-      const basicData = ['YHOO', 'SLB', 'HAL', 'GE', 'GD', 'VZ', 'PG', 'AAL', 'LUV', 'AMD', 'BP', 'C',
-        'WFC', 'CSCO', 'CMG', 'DIS', 'F', 'GM', 'FB', 'GIS', 'IBKC', 'HD', 'JPM', 'K', 'LMT', 'MMM', 'PEP',
-        'PFE', 'PM', 'RCL', 'TEVA', 'WMT', 'XON'];
-
-      let makeEndpoint = '/financials';
-      basicData.forEach((stock) => { makeEndpoint += `/${stock}`; });
-      console.log(makeEndpoint, 'this is your endpoint sire');
-      $http.get(makeEndpoint)
-        .then((resp) => {
-          console.log(resp.data.data, 'this is doge response!');
-          const fixedData = resp.data.data;
-          fixedData.pop();
-          this.stocks = fixedData;
-        }).catch(err => console.error(err));
+    controller: ['$http', '$rootScope', function DashBoardController($http, $rootScope) {
+      //get data from the server of what stocks the user tracks
+      $http.get(`/users/${$rootScope.id}`).then((response) => {
+        console.log(response.data, 'this is a response');
+        const stocksToTrack = response.data.trackedStocks;
+        console.log(stocksToTrack, 'stockies');
+        let makeEndpoint = '/financials';
+        stocksToTrack.forEach((stock) => { makeEndpoint += `/${stock}`; });
+        console.log(makeEndpoint, 'this is your endpoint sire');
+        $http.get(makeEndpoint)
+          .then((resp) => {
+            console.log(resp.data.data, 'this is doge response!');
+            const fixedData = resp.data.data;
+            fixedData.pop();
+            this.stocks = fixedData;
+          }).catch(err => console.error(err));
+      });
     }],
   });
