08a073ad5cde48aeae0eaac1428091c9d1acc3a3
Zachary Bergmann <zbbergma@gmail.com>
2017-04-20 09:46:08 -0500

    add 15 second update functionality to dashboard main for stocks

--- public/app/dashboard/dashboard.component.js
+++ public/app/dashboard/dashboard.component.js
@@ -6,14 +6,17 @@ angular
       const basicData = ['YHOO', 'AAL', 'AMD', 'IBKC', 'FB', 'CSCO', 'SLB', 'HAL', 'GE', 'GD', 'VZ', 'PG', 'LUV', 'BP', 'C',
         'WFC', 'CMG', 'DIS', 'F', 'GM', 'GIS', 'HD', 'JPM', 'K', 'LMT', 'MMM', 'PEP',
         'PFE', 'PM', 'RCL', 'TEVA', 'WMT', 'XON'];
-
-      let makeEndpoint = '/financials';
-      basicData.forEach((stock) => { makeEndpoint += `/${stock}`; });
-      $http.get(makeEndpoint)
-        .then((resp) => {
-          const fixedData = resp.data.data;
-          fixedData.pop();
-          this.stocks = fixedData;
-        }).catch(err => err);
+      const init = () => {
+        setTimeout(init, 15000);
+        let makeEndpoint = '/financials';
+        basicData.forEach((stock) => { makeEndpoint += `/${stock}`; });
+        $http.get(makeEndpoint)
+          .then((resp) => {
+            const fixedData = resp.data.data;
+            fixedData.pop();
+            this.stocks = fixedData;
+          }).catch(err => err);
+      };
+      init();
     }],
   });
